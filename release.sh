7ab3b5610cd192dd4152bc1e374192d4d5598970
Tom Golden <tbjgolden@gmail.com>
2019-04-18 23:23:52 -0700

    Update (2019-04-18 23:23:44)

--- release.sh
+++ release.sh
@@ -2,7 +2,8 @@
 
 DATE=`date '+%Y-%m-%d %H:%M:%S'`
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-cd DIR
+cd $DIR
 rm -f build.zip
 yarn build
-git checkout dev && git add . && git commit -m "Update ($DATE)" && git push && git stash && git checkout master && git stash pop
+zip -r build.zip build
+git checkout dev && git add . && git commit -m "Update ($DATE)" && git push && git checkout mastersh pop
