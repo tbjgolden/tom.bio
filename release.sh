2f9bfdcccdc2cc3ae14efa19e3cc884dd3829471
Tom Golden <tbjgolden@gmail.com>
2019-04-18 23:17:13 -0700

    release script

---
+++ release.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+DATE=`date '+%Y-%m-%d %H:%M:%S'`
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+cd DIR
+rm -f build.zip
+yarn build
+zip -r build.zip build
+git checkout dev && git add . && git commit -m "Update ($DATE)" && git push && git checkout master && 
