5f2f7c370b322a8e45c0c2839277d44cae6e8885
Tom Golden <tbjgolden@gmail.com>
2019-04-18 23:47:34 -0700

    gh-pages

--- release.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-DATE=`date '+%Y-%m-%d %H:%M:%S'`
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-cd $DIR
-rm -f build.zip
-yarn build
-zip -r build.zip build
-git checkout dev && git add . && git commit -m "Update ($DATE)" && git push && git checkout master && mkdir .tmp && mv build.zip .tmp && rm -rf * && mv .tmp/build.zip . && rm -rf .tmp && unzip build.zip && mv build/* . && rm -rf build && git add . && git commit -m "Release ($DATE)" && git push --force && git checkout dev
