bfda7b3fa867464f9f46265beb28c933dc9ce37e
Tom Golden <tbjgolden@gmail.com>
2019-04-18 23:31:49 -0700

    release script

--- release.sh
+++ release.sh
@@ -6,4 +6,4 @@ cd $DIR
 rm -f build.zip
 yarn build
 zip -r build.zip build
-git checkout dev && git add . && git commit -m "Update ($DATE)" && git push && git checkout master
+git checkout dev && git add . && git commit -m "Update ($DATE)" && git push && git checkout master && mkdir .tmp && mv build.zip .tmp && rm -rf * && mv .tmp/build.zip . && rm -rf .tmp && unzip build.zip && mv build/* . && rm -rf build && git add . && git commit -m "Release ($DATE)" && git push --force && git checkout dev
