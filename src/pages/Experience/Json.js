5e309949f4275b8d547113026fd0957e39860b78
Tom Golden <tbjgolden@gmail.com>
2019-06-22 23:03:20 -0700

    obfs-iously

--- src/pages/Experience/Json.js
+++ src/pages/Experience/Json.js
@@ -1,9 +1,27 @@
 import React from "react";
 
-const Json = ({ xp }) => (
-  <pre>
-    <code>{JSON.stringify(xp, null, 2)}</code>
-  </pre>
-);
+const Json = ({ xp }) => {
+  return (
+    <pre>
+      <code>
+        {"{\n  "}
+        {[...Object.entries(xp)].map(([k, v], i, arr) =>
+          ["email", "phone"].includes(k) ? (
+            <React.Fragment>
+              {`"${k}": "`}
+              {window.obfs(v)}
+              {'",\n  '}
+            </React.Fragment>
+          ) : (
+            `${JSON.stringify(k)}: ${JSON.stringify(v, null, 2)
+              .split("\n")
+              .join("\n  ")}${i === arr.length - 1 ? "" : ",\n  "}`
+          )
+        )}
+        {"\n}"}
+      </code>
+    </pre>
+  );
+};
 
 export default Json;
