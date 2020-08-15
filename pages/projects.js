8c4cf5eb2927af2de93ca41435b686b0dc17d70c
Tom Golden <tom@hatchedlabs.com>
2020-08-15 10:36:32 -0500

    projects page

---
+++ pages/projects.js
@@ -0,0 +1,26 @@
+import Container from "@/components/container";
+import Layout from "@/components/layout";
+import { getLayoutData } from "@/lib/api";
+
+export default function Index({ layoutData }) {
+  return (
+    <Layout layoutData={layoutData}>
+      <Container>
+        <section>
+          <p>
+            This page is under construction.
+          </p>
+        </section>
+      </Container>
+    </Layout>
+  );
+}
+
+export const getStaticProps = async () => {
+  const layoutData = await getLayoutData();
+  return {
+    props: {
+      layoutData,
+    },
+  };
+};
