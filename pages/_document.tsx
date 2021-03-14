2ea5bdd0331def63216bf4575a7df14a1a40a2fb
Tom Golden <8162045+tbjgolden@users.noreply.github.com>
2021-03-14 18:41:21 -0500

    feat: updated font and favicon

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -9,10 +9,18 @@ export default class _Document extends Document {
             href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700&display=swap"
             rel="stylesheet"
           />
+          <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png?v=1" />
+          <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png?v=1" />
+          <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png?v=1" />
+          <link rel="manifest" href="/site.webmanifest?v=1" />
+          <link rel="mask-icon" href="/safari-pinned-tab.svg?v=1" color="#5bbad5" />
+          <link rel="shortcut icon" href="/favicon.ico?v=1" />
+          <meta name="theme-color" content="#222222" />
         </Head>
         <body
           style={{
-            fontFamily: "'Inter', sans-serif",
+            fontFamily: "'Manrope VF',Manrope,-apple-system,BlinkMacSystemFont,Roboto,'Helvetica Neue',sans-serif",
+            fontVariationSettings: "'wght' 500",
             visibility: "hidden",
           }}
           className="foszMD"
