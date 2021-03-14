f3b7075fc46863ce1e649183bc2a96a3e391397d
Tom Golden <8162045+tbjgolden@users.noreply.github.com>
2021-03-14 18:54:27 -0500

    fix: ui bugs and favicon issues

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -9,12 +9,12 @@ export default class _Document extends Document {
             href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700&display=swap"
             rel="stylesheet"
           />
-          <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png?v=1" />
-          <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png?v=1" />
-          <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png?v=1" />
-          <link rel="manifest" href="/site.webmanifest?v=1" />
-          <link rel="mask-icon" href="/safari-pinned-tab.svg?v=1" color="#5bbad5" />
-          <link rel="shortcut icon" href="/favicon.ico?v=1" />
+          <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+          <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+          <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+          <link rel="manifest" href="/site.webmanifest" />
+          <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+          <link rel="shortcut icon" href="/favicon.ico" />
           <meta name="theme-color" content="#222222" />
         </Head>
         <body
